f14db532b8c0bbf42dd11ab4dd0a98691c9168b5
alientim <tim@eertmoed.de>
2025-03-04 21:15:38 +0100

    Delete iobroker/run.sh

--- iobroker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-CONFIG_PATH="/addon_configs/iobroker"
-mkdir -p "$CONFIG_PATH"
-ln -sf "$CONFIG_PATH" /opt/iobroker
-chown -R iobroker:iobroker "$CONFIG_PATH"
-exec gosu iobroker node /opt/iobroker/node_modules/iobroker.js-controller/controller.js
